abfbb943982edd0c952d6507311ddac9dd52ed75
Konrad <grabowski.konrad01@gmail.com>
2015-10-25 22:26:02 +0100

    no message

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name youTubeV2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the youTubeV2App
+ */
+angular.module('youTubeV2App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name youTubeV2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the youTubeV2App
+ */
+angular.module('youTubeV2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
